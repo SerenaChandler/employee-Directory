1c60a7abfe43f3ec927922e2cfd69f134a0eb625
Serena Chandler <serenachandler2019@gmail.com>
2021-05-19 17:43:19 -0700

    passed props down to searchbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React, { Component } from "react";
 import Searchbar from "./components/Searchbar";
+import Employee from "./components/Employee"
+import API from "./utils/API";
 import "./App.css";
 
 class App extends Component {
   state = {
     employeeName: "",
+    results: [],
+  };
+
+  componentDidMount() {
+    this.showEmployee();
+  }
+
+  showEmployee = () => {
+    API.getRandomPerson()
+      .then((res) => {
+        console.log(res);
+        this.setState({ results: res.data.results });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    console.log(value);
+    this.setState({
+      [name]: value,
+    });
   };
 
   render() {
     return (
       <div className="App">
-        <Searchbar></Searchbar>
+        <Searchbar
+          employeeName={this.state.employeeName}
+          handleInputChange={this.handleInputChange}
+        />
+        <Employee 
+        results = {this.state.results}
+        />
       </div>
     );
   }
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Form from 'react-bootstrap/Form'
 
-function Searchbar() {
+function Searchbar(props) {
   return( 
     <Form>
  
    
-    <Form.Control type="text" placeholder="Employee name" />
+    <Form.Control value={props.employeeName} name="employeeName"  onChange={props.handleInputChange} type="text" placeholder="Employee name" />
     
    
   </Form>
