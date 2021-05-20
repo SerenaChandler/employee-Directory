cefd0417bb240ffd77aca9588b6be95a92a2d960
Serena Chandler <serenachandler2019@gmail.com>
2021-05-19 17:43:42 -0700

    passed down results prop to employee

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,1 +1,37 @@
 import React from "react";
+import Table from 'react-bootstrap/Table'
+
+function Employee(props) {
+  return( 
+<Table striped bordered hover>
+  <thead>
+    <tr>
+    
+      <th>Image</th>
+      <th> Name</th>
+      <th> Email</th>
+      <th> Phone</th>
+      <th> DOB</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+
+     {props.results.map(result => (
+         <tr>
+         <td><img alt="employee headshot" src={result.picture.thumbnail}/></td>
+         <td>{result.name.first} {result.name.last} </td>
+         <td>{result.email}</td>
+         <td>{result.cell}</td>
+         <td>{result.dob.date}</td>
+       </tr>
+      ))}
+  
+
+  </tbody>
+</Table>
+
+  )
+}
+
+export default Employee
